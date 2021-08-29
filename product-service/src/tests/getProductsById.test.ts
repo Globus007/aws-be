f473d1c1eb7a570e6494ed24dada3809fac12c50
Gleb Stashinsky <g.stashinsky@gmail.com>
2021-08-29 19:21:43 +0300

    feat: add some tests

---
+++ product-service/src/tests/getProductsById.test.ts
@@ -0,0 +1,28 @@
+import {main} from "@functions/getProductsById/getProductById";
+
+describe('Lambda getProductById() should work correct', () => {
+    const event = { pathParameters: { productId: 1 } };
+
+    test('Lambda getProductById() should return string in body', async () => {
+        // @ts-ignore
+        const response = await main(event);
+        // @ts-ignore
+        expect(typeof response.body).toBe('string');
+    });
+
+    test('Lambda getProductById() should return correct status', async () => {
+        // @ts-ignore
+        const response = await main(event);
+        // @ts-ignore
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('Lambda getProductById() should return correct body', async () => {
+        const expectBody = '{"id":2,"title":"iPhone 13","description":"Phone 13, 128 Gb, Black","price":1500}';
+        // @ts-ignore
+        const response = await main(event);
+        // @ts-ignore
+        expect(response.body).toBe(expectBody);
+    })
+
+})
