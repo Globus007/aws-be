19df657698e172a7ac6cde002d89e1ff4203ae48
Gleb Stashinsky <g.stashinsky@gmail.com>
2021-09-08 16:59:37 +0300

    feat: add work with DB

--- product-service/src/components/data.component.ts
+++ product-service/src/components/data.component.ts
@@ -1,15 +1,43 @@
 import { Product } from '../types/types';
-import products from '../products.json';
 import { NotFoundError } from '../utils/errors';
+import { Client, ClientConfig, QueryResult } from 'pg';
 
-export const getAllProducts = async (): Promise<Product[]> => {
-  return products as unknown as Product[];
+const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASS } = process.env;
+
+const dbConfig: ClientConfig = {
+  host: DB_HOST,
+  port: +DB_PORT,
+  database: DB_DATABASE,
+  user: DB_USER,
+  password: DB_PASS,
+  ssl: { rejectUnauthorized: false },
+  connectionTimeoutMillis: 5000,
 };
 
-export const getProduct = async (id: number): Promise<Product> => {
-  const product: Product = products[id] as unknown as Product;
+const executeQuery = async <T>(query: string, params: string[] = []): Promise<QueryResult<T>> => {
+  const client = new Client(dbConfig);
+  try {
+    await client.connect();
+    return await client.query<T>(query, params);
+  } finally {
+    await client.end();
+  }
+};
+
+export const getAllProducts = async (): Promise<Product[]> => {
+  const query = 'SELECT * FROM products p join stocks s on p.id = s.product_id';
+  const res = await executeQuery<Product>(query);
+  return res.rows;
+};
+
+export const getProduct = async (id: string): Promise<Product> => {
+  const query = 'SELECT * FROM products p join stocks s on p.id = s.product_id WHERE id=$1';
+  const res = await executeQuery<Product>(query, [id]);
+
+  const product = res.rows[0];
   if (!product) {
     throw new NotFoundError(`Product with id = ${id} not found`);
   }
+
   return product;
 };
