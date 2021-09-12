650d8082c8402b3a6d357e478fe8dda0d92fb1ed
Gleb Stashinsky <g.stashinsky@gmail.com>
2021-09-12 11:42:31 +0300

    fix: fix return price

--- product-service/src/components/data.component.ts
+++ product-service/src/components/data.component.ts
@@ -1,7 +1,6 @@
 import { PostParams, Product } from '../types/types';
-import { NotFoundError, ValidationError } from '../utils/errors';
+import { NotFoundError } from '../utils/errors';
 import { Client, ClientConfig, QueryResult } from 'pg';
-import validator from 'validator';
 
 const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASS } = process.env;
 
@@ -26,21 +25,21 @@ const executeQuery = async <T>(query: string, params: string[] = []): Promise<Qu
 };
 
 export const getAllProducts = async (): Promise<Product[]> => {
-  const query = `SELECT id, title, description, count
+  const query = `SELECT id, title, description, price, count
                  FROM products p
                           join stocks s on p.id = s.product_id`;
-  const res = await executeQuery<Product>(query);
-  return res.rows;
+  const { rows: products } = await executeQuery<Product>(query);
+  return products;
 };
 
 export const getProduct = async (id: string): Promise<Product> => {
-  const query = `SELECT id, title, description, count
+  const query = `SELECT id, title, description, price, count
                  FROM products p
                           join stocks s on p.id = s.product_id
                  WHERE id = $1`;
-  const res = await executeQuery<Product>(query, [id]);
+  const { rows: products } = await executeQuery<Product>(query, [id]);
 
-  const product = res.rows[0];
+  const product = products[0];
   if (!product) {
     throw new NotFoundError(`Product with id = ${id} not found`);
   }
@@ -48,19 +47,7 @@ export const getProduct = async (id: string): Promise<Product> => {
   return product;
 };
 
-const validateParams = (params: PostParams): void => {
-  const { price, count } = params;
-  if (!validator.isInt(String(price))) {
-    throw new ValidationError(`Price should be integer - ${price}`);
-  }
-  if (!validator.isInt(String(count))) {
-    throw new ValidationError(`Count should be integer - ${count}`);
-  }
-};
-
-// TODO: refactor this method
 export const addProductToDB = async (params: PostParams): Promise<void> => {
-  validateParams(params);
   const { title, price, description, count } = params;
 
   const client = new Client(dbConfig);
@@ -71,12 +58,15 @@ export const addProductToDB = async (params: PostParams): Promise<void> => {
     const queryAddProduct = `INSERT INTO products(title, description, price)
                              VALUES ($1, $2, $3)
                              RETURNING id`;
-    const res = await client.query<Product>(queryAddProduct, [title, description, String(price)]);
+    const { rows: products } = await client.query<Product>(queryAddProduct, [title, description, String(price)]);
 
     const queryAddCount = `INSERT INTO stocks(product_id, count)
                            VALUES ($1, $2)`;
-    await client.query(queryAddCount, [res.rows[0]?.id, String(count)]);
+    await client.query(queryAddCount, [products[0]?.id, String(count)]);
     await client.query('COMMIT');
+  } catch (e) {
+    await client.query('ROLLBACK');
+    throw e;
   } finally {
     await client.end();
   }
