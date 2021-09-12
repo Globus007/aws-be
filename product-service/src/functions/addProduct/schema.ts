bb095708b41ac8d087dd2c5b95127458765f84e4
Gleb Stashinsky <g.stashinsky@gmail.com>
2021-09-12 11:42:45 +0300

    fix: fix validation

--- product-service/src/functions/addProduct/schema.ts
+++ product-service/src/functions/addProduct/schema.ts
@@ -3,8 +3,8 @@ export default {
   properties: {
     title: { type: 'string' },
     description: { type: 'string' },
-    price: { type: 'number' },
-    count: { type: 'number' },
+    price: { type: 'integer', minimum: 1 },
+    count: { type: 'integer', minimum: 1 },
   },
   required: ['title', 'price', 'count'],
 } as const;
